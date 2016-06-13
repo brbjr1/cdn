abcb2064f259c256aed977d500dffe8ea6b094a9
brbjr1 <bbehrens@liquidframeworks.com>
2016-06-13 12:44:52 -0500

    version 9

--- FX_Validate_Access/FX_Validate_Access.js
+++ FX_Validate_Access/FX_Validate_Access.js
@@ -7,6 +7,7 @@ validversions.push('5');
 validversions.push('6');
 validversions.push('7');
 validversions.push('8');
+validversions.push('9');
 var myversion ='8'; //update to latest to change version
 var mysessionId = '';
 
@@ -99,8 +100,8 @@ loadresourcesinorder(loadurls, function()
 
 	j2$(document).ready(function()
 	{
-		//j2$( "#MainContent" ).load( mycdnurl + 'main.htm',  function( response, status, xhr ) 	
-		j2$( "#MainContent" ).load( 'https://rawgit.com/brbjr1/cdn/master/FX_Validate_Access/8/main.htm',  function( response, status, xhr ) 	
+		j2$( "#MainContent" ).load( mycdnurl + 'main.htm',  function( response, status, xhr ) 	
+		//j2$( "#MainContent" ).load( 'https://rawgit.com/brbjr1/cdn/master/FX_Validate_Access/8/main.htm',  function( response, status, xhr ) 	
 		{
 			if ( status == "error" ) 
 			{
